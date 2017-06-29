295ecfb861149ad7aff7c890964b8256875a0816
jihongju <daniel.jihong.ju@gmail.com>
2017-06-29 15:04:16 +0200

    Update voc2011

--- voc2011/train.py
+++ voc2011/train.py
@@ -1,6 +1,10 @@
+import os
+import yaml
 import datetime
 import numpy as np
-from fcn import FCN
+import tensorflow as tf
+import keras.backend as K
+from keras_fcn import FCN
 from voc_generator import PascalVocGenerator, ImageSetLoader
 from keras.callbacks import (
     ReduceLROnPlateau,
@@ -8,7 +12,16 @@ from keras.callbacks import (
     EarlyStopping,
     ModelCheckpoint)
 
-import yaml
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+global _SESSION
+config = tf.ConfigProto(allow_soft_placement=True)
+config.gpu_options.allow_growth = True
+_SESSION = tf.Session(config=config)
+K.set_session(_SESSION)
+
+
 with open("init_args.yml", 'r') as stream:
     try:
         init_args = yaml.load(stream)
@@ -30,29 +43,37 @@ csv_logger = CSVLogger(
     'output/{}_fcn_vgg16.csv'.format(datetime.datetime.now().isoformat()))
 
 
-datagen = PascalVocGenerator(**init_args['pascal_voc_generator']['train'])
+datagen = PascalVocGenerator(image_shape=[500, 500, 3],
+                             image_resample=True,
+                             pixelwise_center=True,
+                             pixel_mean=[104.00699, 116.66877, 122.67892])
+
 
 train_loader = ImageSetLoader(**init_args['image_set_loader']['train'])
 val_loader = ImageSetLoader(**init_args['image_set_loader']['val'])
 
-fcn_vgg16 = FCN(basenet='vgg16', input_shape=(500, 500, 3), num_output=21)
-fcn_vgg16.compile(optimizer='rmsprop',
+fcn_vgg16 = FCN(input_shape=(500, 500, 3), classes=21,
+                weights='imagenet', trainable_encoder=True)
+fcn_vgg16.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy', 'categorical_accuracy'])
 
-flow_args = init_args['pascal_voc_generator']['flow_from_imageset']
-train_flow_args = flow_args.copy()
-train_flow_args['image_set_loader'] = train_loader
-val_flow_args = flow_args.copy()
-val_flow_args['image_set_loader'] = val_loader
 
-fcn_vgg16.fit_generator(datagen.flow_from_imageset(**train_flow_args),
-                        steps_per_epoch=1112,
-                        epochs=100,
-                        validation_data=datagen.flow_from_imageset(
-                            **val_flow_args),
-                        validation_steps=1111,
-                        verbose=1,
-                        max_q_size=100,
-                        callbacks=[lr_reducer, early_stopper, csv_logger])
+fcn_vgg16.fit_generator(
+    datagen.flow_from_imageset(class_mode='categorical',
+                               classes=21,
+                               batch_size=1,
+                               shuffle=True,
+                               image_set_loader=train_loader),
+    steps_per_epoch=1112,
+    epochs=100,
+    validation_data=datagen.flow_from_imageset(class_mode='categorical',
+                                               classes=21,
+                                               batch_size=1,
+                                               shuffle=True,
+                                               image_set_loader=val_loader),
+    validation_steps=1111,
+    verbose=1,
+    max_q_size=100,
+    callbacks=[lr_reducer, early_stopper, csv_logger])
 fcn_vgg16.save('output/fcn_vgg16.h5')
